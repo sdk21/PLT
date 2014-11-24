7986fcf226a453a40a8faaddf892f1f9bec1d3f9
Winnie Narang <wn2146@columbia.edu>
2014-11-23 20:48:02 -0500

    Pretty printing working...

---
+++ Compiler/ast.ml
@@ -0,0 +1,189 @@
+(* Abstract Syntax Tree (AST)
+    - Produced by parser
+    - Consumed by semantic analyzer
+*)
+
+(* Elementary Data Types *)
+type data_type =
+    Int
+  | Float
+  | Comp
+  | Mat
+  | Qub
+
+(* Unary Operators *) 
+type un_op =
+  Not
+  | Re
+  | Im
+  | Norm
+  | Trans
+  | Det
+  | Adj
+  | Conj
+  | Unit
+  | Sin
+  | Cos
+  | Tan
+
+(* Binary Operators *)
+type bi_op =
+  Add
+  | Sub
+  | Mult
+  | Div
+  | Mod
+  | Expn
+  | Tens
+  | Eq
+  | Neq
+  | Lt
+  | Gt
+  | Leq
+  | Geq
+  | Or
+  | And
+  | Xor
+
+(* Expressions *)
+type expr =
+  Lit_int of int
+  | Lit_float of float
+  | Lit_comp of float * float
+  | Qub of expr * int
+  | Mat of expr list list
+  | Id of string
+  | Unop of un_op * expr
+  | Binop of expr * bi_op * expr
+  | Assign of string * expr
+  | Call of string * expr list
+  | Noexpr 
+
+(* Statements *)
+type stmt =
+  Expr of expr
+  | Block of stmt list
+  | If of expr * stmt
+  | For of expr * expr * expr * expr * stmt
+  | While of expr * stmt
+ 
+(* Variables Declaration *)
+type var_decl = 
+  { 
+    var_type : data_type;
+    var_name : string;
+  }
+
+(* Function Declaration *)
+type func_decl = 
+  {
+    ret_type : data_type;
+    ret_name : string;
+    func_name : string;
+    formal_params : var_decl list;
+    locals : var_decl list;
+    body : stmt list;
+  }
+
+(* Program *)
+type program =
+  var_decl list * func_decl list
+
+
+let string_of_word string_of = function 
+    Some(x) -> string_of x 
+  | None -> ""
+
+
+ (* Need to work on expr and stmts *)
+let rec string_of_expr = function
+    Lit_int(n) -> string_of_int n 
+  | Lit_float(n) -> string_of_float n
+  | Lit_comp(f1,f2) -> string_of_float f1 ^ " + i " ^ string_of_float f2
+  | Qub(ex1,n) -> "Qub of " ^ string_of_expr ex1 ^ " and " ^string_of_int n
+  | Mat(exp_list_list) ->  " <Matrix here> "(*String.concat "\n" (List.map string_of_expr exp_list_list) *)
+  | Id(s) -> s
+  | Unop(un1,exp1) -> 
+    (match un1 with
+      Not -> " ! "
+    | Re -> " Re "
+    | Im -> " Im "
+    | Norm -> " Norm "
+    | Trans -> " Trans "
+    | Det -> " Det "
+    | Adj -> " Adj "
+    | Conj -> " Conj "
+    | Unit -> " Unit "
+    | Sin -> " Sin "
+    | Cos -> " Cos "
+    | Tan -> " Tan "
+      ) ^ string_of_expr exp1 
+
+  | Binop(ex1,binop,ex2) -> string_of_expr ex1 ^ 
+    (match binop with 
+    Add -> " + "    | Sub -> " - "     | Mult -> " * " 
+    | Div -> " / "    | Mod -> " % " | Expn -> " Expn " | Tens -> " Tens "
+    | Eq-> " == " | Neq -> " != "    | Lt -> " < "
+    | Leq -> " <= "   | Gt -> " > " | Geq -> " >= "
+    | Xor -> " XOR " | And -> " && "    | Or -> " || "
+    ) ^ string_of_expr ex2
+
+  | Assign(str,expr) -> str ^ "=" ^ string_of_expr expr
+  | Call(str,expr_list) -> "Calling " ^ str ^ "on " ^string_of_exprs expr_list
+  | Noexpr -> ""
+  
+
+
+and string_of_exprs exprs = 
+  String.concat "\n" (List.map string_of_expr exprs) (*==== check this too!========*)
+
+
+let rec string_of_stmt = function
+  Expr(exp1) -> string_of_expr exp1 
+  | Block(stmt_list) -> "{\n" ^ string_of_stmts stmt_list ^ "\n}"
+  | If(expr,stmt) -> "If condition : " ^ string_of_expr expr ^ "\nstatement : " ^ string_of_stmt stmt
+  | For(ex1,ex2,ex3,ex4,stmt) -> "For args : " ^ string_of_expr ex1 ^ " " ^ string_of_expr ex2 ^ " "^ string_of_expr ex3 ^ 
+                                 " "^ string_of_expr ex4 ^ "\nstatement : " ^ string_of_stmt stmt 
+  | While(expr,stmt) -> "While condition : " ^ string_of_expr expr ^ "\nstatement : " ^ string_of_stmt stmt
+
+
+
+and string_of_stmts stmts = 
+  String.concat "\n" (List.map string_of_stmt stmts) 
+
+(* method for printing variable decls *)  
+let string_of_var_decl var_decl = 
+  "var_name: " ^ var_decl.var_name ^ "\nvar_type: " ^ 
+  ( match var_decl.var_type with
+
+    Int -> " int "
+  | Float -> " float "
+  | Comp -> " comp "
+  | Mat -> " mat "
+  | Qub -> " qub "
+  )
+  ^ "\n"
+  
+(* method for printing function decls *)    
+let string_of_fdecl fdecl =
+  "\nfreturn_type: " ^ 
+( match fdecl.ret_type with
+
+    Int -> " int "
+  | Float -> " float "
+  | Comp -> " comp "
+  | Mat -> " mat "
+  | Qub -> " qub "
+  )
+   ^ "\nfreturn_name" ^ fdecl.ret_name ^ "\nfname: "  ^ fdecl.func_name ^  "(" ^
+  String.concat " " (List.map string_of_var_decl fdecl.formal_params) ^ ")\n{\n" ^
+  String.concat " " (List.map string_of_var_decl fdecl.locals) ^ " "^
+  String.concat " " (List.map string_of_stmt fdecl.body) ^
+  "}"
+
+
+(* method for printing program - list of var_decl and func_decl *)  
+let string_of_program (vars, funcs) = 
+  "VARS: \n" ^ String.concat " " (List.map string_of_var_decl vars) ^ " \n\nFUNCTIONS: " ^
+  String.concat "\n" (List.map string_of_fdecl funcs) 
+
--- Compiler/qlc.ml
+++ Compiler/qlc.ml
@@ -3,10 +3,9 @@
     - Produces compiled program
  *)
 
-open Ast
 
-(*
-let rec eval = function 
+
+(*let rec eval = function 
     Lit_int(x) -> 1
     | Mat(x) -> 2
 
@@ -16,3 +15,24 @@ let _ =
   let result = eval expr in
   print_endline (string_of_int result)
 *)
+
+let _ =
+  let lexbuf = Lexing.from_channel stdin in
+  let prog = Parser.expr Scanner.token lexbuf in
+  print_string (Ast.string_of_expr prog)
+
+
+
+(*type action = Ast | Sast  (* | Java | Debug *)
+
+let _ =
+  let action = (* if Array.length Sys.argv > 1 then *)
+    List.assoc Sys.argv.(1) [ ("-a", Ast); ("-s", Sast); (*("-j", Java); ("-d", Debug);*)]
+  (* else Java *) in
+  let lexbuf = Lexing.from_channel stdin in
+  let program = Parser.program Scanner.token lexbuf in
+  match action with 
+  | Ast -> 
+      print_string (Ast.string_of_program program)
+  | Sast -> print_string (Ast.string_of_program program) (* Repeated just to remove error for now *)
+  *)
