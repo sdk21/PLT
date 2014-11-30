7ce03dbec5305089af7174c58b2b2edfc7490ea2
Jonathan Wong <jmw2245@columbia.edu>
2014-11-30 14:46:13 -0800

    Initial merge

--- Compiler/generator.ml
+++ Compiler/generator.ml
@@ -2,16 +2,20 @@ open Sast
 open Printf
 
 (* type_of from Sast *)
-let type_of (a : Sast.expr_wrapper) : Sast.t =
+let type_of (a : Sast.expr_wrapper) : Sast.sdata_type =
     match a with
     | Expr(_,t)-> t
 
-let cpp_from_type (ty: Sast.t) : string =
+let cpp_from_type (ty: Sast.sdata_type) : string =
     match ty with
     | Int -> "int"
     | Float -> "float"
     | Comp -> "complex"
     | Mat -> "Matrix"
+    | Mat_int -> "Matrix"
+    | Mat_float -> "Matrix"
+    | Mat_comp -> "Matrix"
+    | Qub -> "Matrix"
     | Qub_bra -> "Matrix"
     | Qub_ket -> "Matrix"
 
@@ -22,14 +26,22 @@ let rec writeToFile fileName progString =
 and gen_program fileName prog =
     let cppString = writeCpp prog in
     let out = sprintf "
-    #include <Eigen> #include <stdio> #include <cmath> #include <complex>
+    #include <Eigen/Dense>
+    #include <cmath>
+    #include <complex>
+    #include <iostream>
+    #include "constants.h"
+    #include "tensorProduct.h"
+
+    using namespace Eigen;
+    using namespace std;
     %s
     " cppString in 
     writeToFile fileName out;
     out
 
 and writeCpp funcList =
-    let outStr = List.fold_left (fun a b -> a ^ (cpp_funcList b)) "" stmtList in
+    let outStr = List.fold_left (fun a b -> a ^ (cpp_funcList b)) "" funcList in
     sprintf "%s" outStr
 
 and cpp_funcList func =
@@ -38,18 +50,22 @@ and cpp_funcList func =
     and cppFName = func.sfunc_name
     and cppFParam = cppVarDecl func.sformal_params
     and cppFBody = cppStmt func.sbody in
+    and cppLocals = cppLocalVar func.slocals
     let cppfunc = sprintf "
     %s %s (%s){
         %s
-        return %s
+        return  %s
     }
     " cppRtnType cppFName cppFParam cppFBody cppRtnValue
 
 and cppReturnType  = function
 
+
 and cppReturnValue = function
 
-and cppVarDecl = function
+and cppVarDecl vardeclist =
+   let varDecStr = List.fold_left (fun a b -> a ^ (cppVar b)) "" vardeclist in
+   sprintf "%s" varDecStr 
 
 and cppExpr = function
   Binop(expr1, op, expr2, _) -> writeBinop expr1 op expr2
@@ -67,12 +83,16 @@ and cppExpr = function
   *)
 
 
-and cppStmt = function 
-      If(expr , stmt) -> writeIfStmt expr stmt
+and cppStmt = function
+    Sexpr(sexpr) -> writeExpr sexpr
+    | Block (stmtlist) -> 
+            List.fold_left (fun a b -> a ^ (cppStmt b)) "" stmtlist    
+    | If(expr , stmt) -> writeIfStmt expr stmt
     | For(var,init, final, increment, stmt) -> 
             writeForStmt var init final increment stmt
-    | While(expr, stmt) -> writeWhileStmt expr stmt  
+    | While(expr, stmt) -> writeWhileStmt expr stmt
 
+(*
 and writeIfStmt expr stmt = 
 	let cond = cppExpr expr 
 	and body = writeCpp stmt in (*probably not right function call*)
@@ -81,6 +101,20 @@ and writeIfStmt expr stmt =
 			%s
 		} " cond body
 	in 
+
+and writeForStmt var init final increment stmt =
+    let varname = var 
+    and initvalue = string_of_int init
+    and finalvalue = string_of_int final
+    and incrementval = string_of_int increment
+    and stmtbody = cppStmt stmt
+    in
+    sprintf "
+    for (int %s = %s; %s < %s ; %s = %s + %s){
+        %s
+        }" varname initvaluevarname finalvalue varname varname incrementval stmtbody
+	*)
+
 
 and writeBinop expr1 op expr2 = 
     let e1 = cppExpr expr1 and e2 = cppExpr expr2 in 
@@ -102,7 +136,3 @@ and writeBinop expr1 op expr2 =
 		| And 	-> sprintf "%s && %s" e1 e2
 		(*| Xor 	-> sprintf "%s ^ %s" e1 e2*)
 	in binopFunc e1 op e2
-
-
-
-
