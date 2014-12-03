7b36815b897b05857f6a783f5d3f48c788cd91e7
Winnie Narang <wn2146@columbia.edu>
2014-12-03 18:34:17 -0500

    Semantic checks error output formatted

--- Compiler/test/semantic_test.sh
+++ Compiler/test/semantic_test.sh
@@ -3,25 +3,26 @@
 
 SCAN="../qlc -s" 
 
-#files="SemanticFailures/*.ql"
-files="SemanticSuccess/*.ql"
+files="SemanticFailures/*.ql"
+#files="SemanticSuccess/*.ql"
 
 
 SemanticCheck() {
-eval "$SCAN $1" 2> test.out 1> test.null
+eval "$SCAN $1" 1>> test.null 2>> test.out 
 wc test.out | awk '{print $1}' 
 }
 
 
+rm -f test.out test.null
+
 for file in $files
 do
 errors=$(SemanticCheck $file)
-SemanticCheck $file
 if [ $errors == 0 ]
 then
 echo "Test: " $file " passed."
 else
 echo $file " failed to pass."
-#exit 1
 fi
 done
+
