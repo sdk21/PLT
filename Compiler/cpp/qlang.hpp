63fd218e382ee883fc9a6c361e5a64389e3d76d3
Jonathan Wong <jmw2245@columbia.edu>
2014-12-12 15:32:01 -0800

    changed const I to IDT

--- Compiler/cpp/qlang.hpp
+++ Compiler/cpp/qlang.hpp
@@ -7,7 +7,7 @@ using namespace std;
 //CONSTANTS
 static const Matrix2cf H = (Matrix2cf() << 1/sqrt(2), 1/sqrt(2),
 				1/sqrt(2), -1/sqrt(2)).finished();
-static const Matrix2cf I = Matrix2cf::Identity();
+static const Matrix2cf IDT = Matrix2cf::Identity();
 static const Matrix2cf X = (Matrix2cf() << 0, 1, 1, 0).finished();
 static const Matrix2cf Y = (Matrix2cf() << 0, -std::complex<float>(0,1),
 				std::complex<float>(0,1), 0).finished();
